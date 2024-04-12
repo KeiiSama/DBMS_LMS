6616b3ee56efb5692cc28d48ec039641ac10a59f
Keii <145163121+KeiiSama@users.noreply.github.com>
2024-04-12 13:31:36 +0700

    new commit

--- Backend/src/models/book.model.js
+++ Backend/src/models/book.model.js
@@ -1,44 +1,57 @@
 const pool = require("../loaders/db");
 
-class Book {
-    async getAllBook(params) {
-        var query = `SELECT * FROM BOOK`;
-        const result = await pool.query(query) ;
-        return result[0] ;
+// class Book {
+//   async getAllBook(params) {
+//     const result = "SELECT * FROM sach";
+//     db.query(sql, callback);
+//   }
+// }
+const getAllBook = (callback) => {
+  const sql = "SELECT * FROM sach";
+  pool.query(sql, (error, results) => {
+    if (error) {
+      console.error(error);
+      callback(error, null);
+    } else {
+      callback(null, results);
     }
+  });
+};
 
-    async create(newBook) {
-        var query = `INSERT INTO BOOK(title, price, author) VALUES (?, ?, ?)`;
-        const {title, price, author} = newBook
-        try {
-            const res = await pool.query(query, [title, price, author])
+module.exports = { getAllBook };
 
-            return res[0]
-        } catch (error) {
-            console.error("Error in create():", error);
-        }
-    }
+//   async create(newBook) {
+//     var query = `INSERT INTO BOOK(title, price, author) VALUES (?, ?, ?)`;
+//     const { title, price, author } = newBook;
+//     try {
+//       const res = await pool.query(query, [title, price, author]);
 
-    async update(id, book) {
-        var query = `UPDATE BOOK SET title = ?, price = ?, author = ? WHERE id = ?`;
-        const { title, price, author } = book;
-        try {
-            const res = await pool.query(query, [title, price, author, id]);
-            return res[0];
-        } catch (error) {
-            console.error("Error in update():", error);
-        }
-    }
+//       return res[0];
+//     } catch (error) {
+//       console.error("Error in create():", error);
+//     }
+//   }
 
-    async delete(id) {
-        var query = `DELETE FROM BOOK WHERE id = ?`;
-        try {
-            const res = await pool.query(query, [id]);
-            return res[0];
-        } catch (error) {
-            console.error("Error in delete():", error);
-        }
-    }
-}
+//   async update(id, book) {
+//     var query = `UPDATE BOOK SET title = ?, price = ?, author = ? WHERE id = ?`;
+//     const { title, price, author } = book;
+//     try {
+//       const res = await pool.query(query, [title, price, author, id]);
+//       return res[0];
+//     } catch (error) {
+//       console.error("Error in update():", error);
+//     }
+//   }
 
-module.exports = Book;
+//   async delete(id) {
+//     var query = `DELETE FROM BOOK WHERE id = ?`;
+//     try {
+//       const res = await pool.query(query, [id]);
+//       return res[0];
+//     } catch (error) {
+//       console.error("Error in delete():", error);
+//     }
+//   }
+// }
+
+// module.exports = Book;
